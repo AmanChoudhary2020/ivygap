6f4f24fd2d025628b3f8f1b07c4f3d2972728866
Aman Choudhary <amanch@umich.edu>
2023-11-07 12:45:50 -0500

    tiling working on test image

--- main.py
+++ main.py
@@ -1,44 +1,71 @@
 import numpy as np
-from typing import Iterable
+from PIL import Image
+from typing import List
+import os
+import shutil
 
-'''
-
-tile size: 2x2
-
-x x x 
-x x x
-x x x 
-x x x 
-'''
-
-def coordinate_generator(image_size: tuple[int, int], tile_size: tuple[int, int], slide_x: int, slide_y: int) -> Iterable[tuple[int, int]]:
-    image_width, image_height = image_size
+def coordinate_generator(image_size: tuple[int, int], tile_size: tuple[int, int], slide_x: int, slide_y: int) -> List[tuple[int, int]]:
+    image_width, image_height, _ = image_size
     tile_width, tile_height = tile_size
+
+    coordinates = []
 
     for y in range(0, image_height - tile_height + 1, slide_y):
         for x in range(0, image_width - tile_width + 1, slide_x):
-            yield (x, y)
+            coordinates.append((x,y))
+
+    return coordinates
 
 def tile_generator(image, tile_size, coordinates):
     tile_width, tile_height = tile_size
-    image_width, image_height = image.shape
+    tiles = []
 
     for x, y in coordinates:
-        if (x + tile_width) >= image_width or (y + tile_height) >= image_height:
-            print("invalid")
-        else:
-            tile = np.array(image[y:y + tile_height, x:x + tile_width])
-            yield tile
+        tiles.append(np.array(image[y:y + tile_height, x:x + tile_width, :]))
+    
+    tiles = np.array(tiles)
+    return tiles
 
-image_size = (800, 800)  
-tile_size = (100, 100)   
-slide_x = 50             
-slide_y = 50           
+def save_tiles_as_images(tiles, output_dir):
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    
+    os.makedirs(output_dir)
+    
+    for i, tile in enumerate(tiles):
+        tile_image = Image.fromarray(tile)
+        output_file_path = os.path.join(output_dir, f"tile_{i}.png")
+        tile_image.save(output_file_path)
 
-image = np.random.rand(image_size[0], image_size[1])
-coordinates = coordinate_generator(image_size, tile_size, slide_x, slide_y)
-tiles = tile_generator(image, tile_size, coordinates)
+def main():
+    '''
+    Dummy Image:
+    1  2  3  13
+    4  5  6  14
+    7  8  9  15
+    10 11 12 16 
+    '''
+    # image = np.array([[1,2,3,13],[4,5,6,14],[7,8,9,15],[10,11,12,16]]) # dummy image
 
-for tile in tiles:
-    print(tile.shape)
-    print(tile)
+    # Load image
+    image = np.array(Image.open("img.png"))
+    image = image.reshape(image.shape[0], image.shape[1], -1)
+    print(f"Image shape: {image.shape}")
+
+    image_size = image.shape
+    tile_size = (100,100)            
+
+    # Generate coordinates
+    coordinates = coordinate_generator(image_size, tile_size, slide_x=50, slide_y=50)
+    # Generate tiles using coordinates
+    tiles = tile_generator(image, tile_size, coordinates)
+
+    # Save tiles to output directory
+    output_dir = "output_tiles"
+    save_tiles_as_images(tiles, output_dir)
+
+
+
+if __name__ == "__main__":
+    main()
+
